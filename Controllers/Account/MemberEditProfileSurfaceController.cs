2803fbc6c6881ed823d6828d1845678db0ae0591
Steve Morgan <steve@siempresolutions.co.uk>
2016-04-27 16:32:23 +0100

    Edit profile not complete #6
    Added in the edit profile fixes.

--- Controllers/Account/MemberEditProfileSurfaceController.cs
+++ Controllers/Account/MemberEditProfileSurfaceController.cs
@@ -18,7 +18,67 @@ namespace UmbracoMembers.Controllers
         [ActionName("MemberEditProfileRenderForm")]
         public ActionResult MemberEditProfileRenderForm()
         {
-            return PartialView("Account/MemberEditProfile", new MemberRegisterModel());
+            var memberService = Services.MemberService;
+            var model = new MemberRegisterModel();
+            var curMember = memberService.GetById(Members.GetCurrentMemberId());
+
+            model.FirstName = curMember.GetValue<string>("firstname");
+            model.LastName = curMember.GetValue<string>("lastname");
+            model.MailingListInclude = curMember.GetValue<bool>("mailingListInclude");  // this is not doing anything - replace with your mailchimp integration!
+
+            return PartialView("Account/MemberEditProfile", model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken()]
+        [ActionName("MemberEditProfile")]
+        public ActionResult MemberEditProfile(MemberEditProfileModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var memberService = Services.MemberService;
+                var member = Members.GetCurrentMember();
+
+                // Get an editable curMember
+                var curMember = memberService.GetById(member.Id);
+
+                // save their password if they provide an update
+                if (!(string.IsNullOrEmpty(model.Password)) && model.Password == model.Password2)
+                {
+
+                    if (model.Password.Length >= 8 && model.Password.Length <= 36)
+                    {
+                        memberService.SavePassword(curMember, model.Password);
+                    }
+                    else
+                    {
+                        TempData["Status"] = "Your new password must be between 8 and 36 characters long";
+                        return RedirectToCurrentUmbracoPage();
+                    }
+                }
+
+                curMember.SetValue("firstName", model.FirstName);
+                curMember.SetValue("lastName", model.LastName);
+                
+                // if you were to change the members email address you'd want to validate it first
+                // TODO
+
+                // here you should do your Mailchimp integration / or whatever
+                curMember.SetValue("mailingListInclude", model.MailingListInclude);
+
+                // remember to save
+                memberService.Save(curMember);
+
+                @TempData["Success"] = true;
+                TempData["Status"] = "Your account has been updated";
+                return RedirectToCurrentUmbracoPage();
+            }
+            else
+            {
+                // model is invalid
+                TempData["Status"] = "Please ensure you've provided all the required information.";
+                return CurrentUmbracoPage();
+            }
         }
 
     }
---
+++ Models/Account/MemberEditProfileModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Umbraco.Web;
+using Umbraco.Web.Models;
+
+namespace UmbracoMembers.Models
+{
+    public class MemberEditProfileModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public bool MailingListInclude { get; set; }
+        [Required(AllowEmptyStrings = true)]
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
+        // [StringLength(36, MinimumLength = 8, ErrorMessage = "The password must be between 8 and 36 characters long.")]
+        //    [RegularExpression(@"^(?=.{8})(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$", ErrorMessage = "The password must be between 8 and 36 characters long and contain at least one special character")]
+        public string Password { get; set; }
+        [Required(AllowEmptyStrings = true)]
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
+        [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string Password2 { get; set; }
+    }
+}
